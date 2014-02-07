e72040498efd24b179e60862476f3a278abd81e0
Paige Ponzeka <pponzeka@yext.com>
2014-02-07 18:45:04 -0500

    Intial commit of files

---
+++ javascripts/process.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  var depthChart = [];
+  generateRoster();
+  generateDepthChart();
+
+
+
+  function generateRoster(){
+    var source   = $('#roster-template').html();
+    var template = Handlebars.compile(source);
+    var html = template(players);
+    $('.js-roster-container').append(html);
+  }
+
+  function generateDepthChart() {
+    $.each(players.players, function(){
+      var player = this;
+      $.each(this.positions, function(){
+        var currentPosition = '' + this;
+        // intialize the position if it hasn't been intialized yet
+        if(depthChart[currentPosition] == undefined) {
+          depthChart[currentPosition] = [];
+        }
+        depthChart[currentPosition].push(player.name); 
+
+      });
+    });
+      appendDepthChart();
+  }
+
+  function appendDepthChart(){
+    var depthChartObject = [];
+    $.each(positions, function(){
+
+      var position = "" + this;
+      var positionObj = {name: position, players: depthChart[position]}; 
+      depthChartObject.push(positionObj);
+    });
+
+    var source   = $('#depth-chart-template').html();
+    var template = Handlebars.compile(source);
+    var html = template({positions : depthChartObject});
+    $('.js-depth-chart-container').append(html);
+  }
+});
---
+++ javascripts/roster.js
@@ -0,0 +1,18 @@
+var players = {players : [
+  {name: "Paige Ponzeka", positions: ["1B", "3B", "P"]},
+  {name: "Ricky Cratty", positions: ["2B", "C"]},
+  {name: "Andrew Flatgard", positions: ["RC", "RF", "LC", "C"]},
+  {name: "William King", positions: ["RC", "RF", "C"]},
+  {name: "Michael Burns", positions:["1B", "LC", "RC"]},
+  {name: "Patrick Marley", positions:["1B"]},
+  {name: "Stephanie Merireles", positions: ["1B", "3B", "SS"]},
+  {name: "Jose Collazo", positions: ["2B"]},
+  {name:"Whitney Liriano", positions:["RC", "RF", "LC", "LF", "2B"]},
+  {name: "Krystal Gonzalez", positions:["RC", "RF", "LC", "LF", "2B"]},
+  {name: "Amanda Babine", positions:["SS", "2B", "3B"]},
+  {name: "Kate Sarrantonio", positions:["2B", "LF", "LC"]},
+  {name: "Lauren Holmes", positions:["RF", "RC", "C"]},
+  {name: "Emily Sperber", positions:["LC", "LF", "RC", "RF"]}
+  ]};
+
+var positions = ["P", "C", "1B", "2B", "SS", "3B", "LF", "LC", "RC", "RF"];
